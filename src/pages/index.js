3f6a1962db0a67e819693ffc2e7d26b7991265fa
Charley McGowan <glitchwizard@gmail.com>
2020-05-20 19:11:32 -0700

    started Part 2, which asked to remove then recreate a new repo, but I'm just making it a branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,3 @@
-import React from "react";
-import {Link} from "gatsby";
+import React from "react"
 
-export default function Home() {
-    return <div style = {
-        {
-            color: `purple`
-        }
-    } >
-        <Link to="/contact/" > Contact </Link>
-
-        <h1> Hello Gatsby! </h1>
-        <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/640x480" alt="A random pic"/>
-        </div>
-}
+export default () => <div>Hello world!</div>
