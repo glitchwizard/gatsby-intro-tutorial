416b3bba79f8474bb1e460ecde546473d82cf448
Charley McGowan <glitchwizard@gmail.com>
2020-05-22 00:45:05 -0700

    Completed Part 3

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default function About() {
+    return <Layout>
+        <h1>About Me</h1>
+        <p>
+            I'm good enough, I'm smart enough, and gosh darnit, people like me!
+        </p>
+    </Layout>
+}
